d9733aa4cc08d68692586bec89d2b4fa9bbc49ed
İbrahim Ethem Sancar <ibrahimethemsancar@gmail.com>
2021-11-06 14:54:57 +0300

    install react-router and add new components

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,13 +2,29 @@ import React from 'react';
 import './App.css';
 import Head from './components/Head';
 import './index.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
+import HomeImg from './components/HomeImg/HomeImg';
 
 
 
 function App() {
   return (
-    
-      <Head/>
+    <Router>
+
+      <Switch>
+        <Route path="/">
+        <Head/>
+          <HomeImg/>
+        </Route>
+      </Switch>
+    </Router>
+
+      
     
   
       
---
+++ Frontend/src/components/HomeImg/HomeImg.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HomeImg.css'
+export default function HomeImg() {
+    return (
+        <div className='HomeImgDiv'>
+            <picture><img width="1200" height="337" className="sf-hb-image-view-Kyv7r undefined hb-image-view__image" src="https://images.hepsiburada.net/banners/s/0/1200-337/marketingbillboard_20211105132045.png"/></picture>
+        </div>
+    )
+}
